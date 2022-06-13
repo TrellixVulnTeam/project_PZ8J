a54a689a523d7bde09cded1000ffe78b6bcb6de7
시냥이 <snyvv@naver.com>
2022-06-13 21:36:44 +0900

    그림판 선그리기

--- painting-board/dist/index.de5c0784.js
+++ painting-board/dist/index.de5c0784.js
@@ -1,3 +1,79 @@
-console.log("\uADF8\uB9BC\uD310");
+class PaintingBoard {
+    MODE = "NONE";
+    isMouseDown = false;
+    constructor(){
+        this.assignElement();
+        this.initContext();
+        this.addEvent();
+    }
+    assignElement() {
+        this.containerEl = document.getElementById("container");
+        this.canvasEl = this.containerEl.querySelector("#canvas");
+        this.toolbarEl = this.containerEl.querySelector("#toolbar");
+        this.brushEl = this.toolbarEl.querySelector("#brush");
+        this.colorPickerEl = this.toolbarEl.querySelector("#colorPicker");
+        this.brushPanelEl = this.containerEl.querySelector("#brushPanel");
+        this.brushSliderEl = this.brushPanelEl.querySelector("#brushSize");
+        this.brushSizePreviewEl = this.brushPanelEl.querySelector("#brushSizePreview");
+    }
+    initContext() {
+        this.context = this.canvasEl.getContext("2d"); // canvas 초기화
+    }
+    addEvent() {
+        this.brushEl.addEventListener("click", this.onClickbrush.bind(this));
+        this.canvasEl.addEventListener("mousedown", this.onMouseDown.bind(this));
+        this.canvasEl.addEventListener("mousemove", this.onMouseMove.bind(this));
+        this.canvasEl.addEventListener("mouseup", this.onMouseUp.bind(this));
+        this.brushSliderEl.addEventListener("input", this.onChangeBrushSize.bind(this));
+        this.colorPickerEl.addEventListener("input", this.onChangeColor.bind(this));
+        this.canvasEl.addEventListener("mouseout", this.onMouseOut.bind(this));
+    }
+    onMouseOut() {
+        if (this.MODE === "NONE") return;
+        this.isMouseDown = false;
+    }
+    onChangeColor(event) {
+        this.brushSizePreviewEl.style.backgroundColor = event.target.value;
+    }
+    onChangeBrushSize(event) {
+        this.brushSizePreviewEl.style.width = `${event.target.value}px`;
+        this.brushSizePreviewEl.style.height = `${event.target.value}px`;
+    }
+    getMousePosition(event) {
+        const boundaries = this.canvasEl.getBoundingClientRect();
+        return {
+            x: event.clientX - boundaries.left,
+            y: event.clientY - boundaries.top
+        };
+    }
+    onClickbrush(event) {
+        const isActive = event.currentTarget.classList.contains("active");
+        this.MODE = isActive ? "NONE" : "BRUSH";
+        this.canvasEl.style.cursor = isActive ? "default" : "crosshair";
+        this.brushPanelEl.classList.toggle("hide");
+        this.brushEl.classList.toggle("active");
+    }
+    onMouseDown(event) {
+        if (this.NONE === "NONE") return;
+        this.isMouseDown = true;
+        const currentPosition = this.getMousePosition(event);
+        this.context.beginPath();
+        this.context.moveTo(currentPosition.x, currentPosition.y);
+        this.context.lineCap = "round";
+        this.context.strokeStyle = this.colorPickerEl.value;
+        this.context.lineWidth = this.brushSliderEl.value;
+    }
+    onMouseMove(event) {
+        if (!this.isMouseDown) return;
+        const currentPosition = this.getMousePosition(event);
+        this.context.lineTo(currentPosition.x, currentPosition.y);
+        this.context.stroke();
+    }
+    onMouseUp() {
+        if (this.MODE === "NONE") return;
+        this.isMouseDown = false;
+    }
+}
+new PaintingBoard();
 
 //# sourceMappingURL=index.de5c0784.js.map
