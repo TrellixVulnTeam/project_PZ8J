a528965b6c0487aa99d35881e232b73b556d84ad
시냥이 <snyvv@naver.com>
2022-06-18 21:27:48 +0900

    sortable 구현

---
+++ sortable/src/TestItem/TestItem.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./TestItem.css";
+
+const TestItem = ({ data, index }) => {
+  return (
+    <div>
+      <div className="test-item">
+        <div>
+          <p>content: {data.content}</p>
+          <p>index: {index}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TestItem;
